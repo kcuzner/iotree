61cdbc7c270879a12c4e3380a03f8d2de21d2fd5
Kevin Cuzner <kevin@kevincuzner.com>
2018-12-16 00:40:24 -0600

    Created initial ledstring and pixel structure
    I'm going to have the users create the pixel lists and download them to
    the raspi.

--- raspi/ws2811.py
+++ raspi/ws2811.py
@@ -1,11 +1,40 @@
 import spidev
 import random
 from colorsys import hsv_to_rgb
-
-STEP = 0.001
+import itertools
 
 def fuzzy_equals(a, b, margin):
     return abs(a - b) < margin
+
+class Pixel(object):
+    def next_color(self):
+        return (0, 0, 0)
+
+class RandomPixel(Pixel):
+    def __init__(self, step):
+        self.step = step
+        self.target = random.random()
+        self.hue = self.target
+    def next_color(self):
+        if fuzzy_equals(self.hue, self.target, self.step):
+            self.target = random.random()
+        elif self.hue < self.target:
+            self.hue += self.step
+        else:
+            self.hue -= self.step
+        return tuple([int(v * 255) for v in hsv_to_rgb(self.hue, 1, 1)])
+
+class LedString(object):
+    def __init__(self, length):
+        self.pixels = []
+        self.buffer = [0] * length * 3
+    def animate(self):
+        colors = list([p.next_color() for p in self.pixels])
+        for i, color in enumerate(itertools.cycle(colors)):
+            if i*3 >= len(self.buffer):
+                break
+            self.buffer[i*3:(i+1)*3] = color
+        return self.buffer
 
 def main():
     spi = spidev.SpiDev()
@@ -13,20 +42,11 @@ def main():
     spi.max_speed_hz = 400000
     spi.mode = 0b01
 
-    targets = list([random.random() for _ in range(0, 50)])
-    hues = list(targets)
+    leds = LedString(50)
+    leds.pixels = list([RandomPixel(0.002) for _ in range(0, 2)])
 
     while True:
-        for i, target in enumerate(targets):
-            if fuzzy_equals(hues[i], target, STEP):
-                targets[i] = random.random()
-            elif hues[i] < target:
-                hues[i] += STEP
-            else:
-                hues[i] -= STEP
-        rgb = list([hsv_to_rgb(h, 1, 1) for h in hues])
-        buf = [int(v * 255) for v in sum(rgb, ())]
-        spi.xfer(buf)
+        spi.xfer(leds.animate())
 
 if __name__ == '__main__':
     main()
