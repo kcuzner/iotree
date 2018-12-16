495e61107881abe49aa6bebd5d5f0ed7f08bc72b
Kevin Cuzner <kevin@kevincuzner.com>
2018-12-15 23:21:20 -0600

    Moved color changing logic into python completely
    It looks *so* much better now that its just moving fully saturated hues.
    Doing that on the microcontroller was going to be a real hassle.

--- raspi/ws2811.py
+++ raspi/ws2811.py
@@ -2,6 +2,11 @@ import spidev
 import random
 from colorsys import hsv_to_rgb
 
+STEP = 0.001
+
+def fuzzy_equals(a, b, margin):
+    return abs(a - b) < margin
+
 def main():
     spi = spidev.SpiDev()
     spi.open(0, 0)
@@ -12,6 +17,13 @@ def main():
     hues = list(targets)
 
     while True:
+        for i, target in enumerate(targets):
+            if fuzzy_equals(hues[i], target, STEP):
+                targets[i] = random.random()
+            elif hues[i] < target:
+                hues[i] += STEP
+            else:
+                hues[i] -= STEP
         rgb = list([hsv_to_rgb(h, 1, 1) for h in hues])
         buf = [int(v * 255) for v in sum(rgb, ())]
         spi.xfer(buf)
